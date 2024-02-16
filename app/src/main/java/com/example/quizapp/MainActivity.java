dce16843f0563e48616b31dd43fb5dcda839b5b7
student <dhirajchavan355@gmail.com>
2024-02-17 00:34:45 +0530

    added anima

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
@@ -13,6 +14,7 @@ import com.example.quizapp.data.AnswerListAsyncResponse;
 import com.example.quizapp.data.Respository;
 import com.example.quizapp.databinding.ActivityMainBinding;
 import com.example.quizapp.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private int currentQuestionIndex=0;
+    List<Question> questionList;
 
 
 
@@ -29,13 +32,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
-        List < Question> questions;
-        new Respository().getQuestions(questionArrayList ->
-                binding.questiontextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer())
-        );
+
+       questionList= new Respository().getQuestions(questionArrayList ->{
+                binding.questiontextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+           updateCounter(questionArrayList);
+
+               }
+       );
+
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex=(currentQuestionIndex +1 ) %questionList.size();
+            updateQuestion();
+
+        });
+        binding.buttonTrue.setOnClickListener(view -> {
+            cheakAnswer(true);
+
+        });
+        binding.buttonFalse.setOnClickListener(view -> {
+            cheakAnswer(false);
+        });
 
 
 
 
     }
+
+    private void cheakAnswer(boolean userchosecorrect) {
+        boolean answer=questionList.get(currentQuestionIndex).isAnswertrue();
+        int snackmessageid=0;
+        if(userchosecorrect==answer){
+            snackmessageid=R.string.correct_answer;
+        }else{
+            snackmessageid=R.string.false_answer;
+        }
+        Snackbar.make(binding.cardView,snackmessageid,Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void updateCounter(ArrayList<Question> questionArrayList) {
+        binding.textViewOutOf.  setText(String.format(getString(R.string.text_formatted) ,
+                currentQuestionIndex, questionArrayList.size()));
+    }
+
+    private void updateQuestion() {
+        String question= questionList.get(currentQuestionIndex).getAnswer();
+        binding.questiontextview.setText(question);
+        updateCounter((ArrayList<Question>) questionList);
+    }
+
 }
